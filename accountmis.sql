de760ff0db25d7a6f66732edae398a6da73e5f19
Shoufves <3144589869@qq.com>
2026-07-07 14:57:14 +0800

    Initial Version

---
+++ accountmis.sql
@@ -0,0 +1,74 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 127.0.0.1
+Source Server Version : 50720
+Source Host           : localhost:3306
+Source Database       : accountmis
+
+Target Server Type    : MYSQL
+Target Server Version : 50720
+File Encoding         : 65001
+
+Date: 2022-06-09 09:36:51
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for category
+-- ----------------------------
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of category
+-- ----------------------------
+INSERT INTO `category` VALUES ('2', '吃');
+INSERT INTO `category` VALUES ('3', '喝');
+INSERT INTO `category` VALUES ('4', '玩');
+INSERT INTO `category` VALUES ('5', '乐');
+INSERT INTO `category` VALUES ('6', '旅行');
+
+-- ----------------------------
+-- Table structure for config
+-- ----------------------------
+DROP TABLE IF EXISTS `config`;
+CREATE TABLE `config` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `key_` varchar(255) DEFAULT NULL,
+  `value` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of config
+-- ----------------------------
+INSERT INTO `config` VALUES ('1', 'budget', '5000');
+INSERT INTO `config` VALUES ('2', 'mysqlPath', 'C:\\Program Files\\MySQL\\MySQL Server 5.7');
+
+-- ----------------------------
+-- Table structure for record
+-- ----------------------------
+DROP TABLE IF EXISTS `record`;
+CREATE TABLE `record` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `spend` int(11) DEFAULT NULL,
+  `cid` int(11) DEFAULT NULL,
+  `comment` varchar(255) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_record_category` (`cid`),
+  CONSTRAINT `fk_record_category` FOREIGN KEY (`cid`) REFERENCES `category` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of record
+-- ----------------------------
+INSERT INTO `record` VALUES ('1', '100', '2', '麦当劳', '2022-03-17');
+INSERT INTO `record` VALUES ('2', '300', '5', '乐园', '2022-03-19');
+INSERT INTO `record` VALUES ('3', '222', '5', '', '2022-03-19');
